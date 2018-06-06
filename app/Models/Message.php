0f1d095a1b72d8ed87d2db25e541cecdf3c05156
deepika <deepika.vinchurkar@gmail.com>
2018-06-06 12:57:39 +0530

    Initial commit

---
+++ app/Models/Message.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class Message extends Model
+{
+	// app/Message.php /** * Fields that are mass assignable * * @var array */
+	protected $fillable = ['message'];
+    //
+
+	public function user()
+	{ 
+		return $this->belongsTo(User::class);
+	}
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //to resolve migration error, 'specified key was too long' add following line
+        \Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
